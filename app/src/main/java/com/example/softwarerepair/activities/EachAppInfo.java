c7698d4f6fba33ff695f2bdf0a11a2bdeddbbcad
umarali07 <66766817+umarali07@users.noreply.github.com>
2020-08-08 15:40:20 +0500

    06/08 fragment info 9

--- app/src/main/java/com/example/softwarerepair/activities/EachAppInfo.java
+++ app/src/main/java/com/example/softwarerepair/activities/EachAppInfo.java
@@ -1,9 +1,12 @@
 package com.example.softwarerepair.activities;
 
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,11 +20,13 @@ import com.example.softwarerepair.utils.TimeUtil;
 
 public class EachAppInfo extends AppCompatActivity {
 
-    ApplicationUtility applicationUtility;
-    TimeUtil timeUtil ;
-    MathCalculationsUtil mathCalculationsUtil;
-    TextView tvAppName,tvVersion,tvSize,tvInstallTime,tvUpdateTime;
-    ImageView ivAppIcon;
+    private ApplicationUtility applicationUtility;
+    private TimeUtil timeUtil;
+    private MathCalculationsUtil mathCalculationsUtil;
+    private TextView tvAppName, tvVersion, tvSize, tvInstallTime, tvUpdateTime;
+    private ImageView ivAppIcon;
+    private LinearLayout llOpenApp, llOpenSetting, llCheckOnStore;
+    private String pkgName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,17 +39,20 @@ public class EachAppInfo extends AppCompatActivity {
         tvSize = findViewById(R.id.tv_size);
         tvInstallTime = findViewById(R.id.install_time_tv);
         tvUpdateTime = findViewById(R.id.update_time_tv);
+        llOpenApp = findViewById(R.id.open_app_ll);
+        llCheckOnStore = findViewById(R.id.check_store_ll);
+        llOpenSetting = findViewById(R.id.open_setting_ll);
 
         applicationUtility = new ApplicationUtility(this);
         timeUtil = new TimeUtil();
         mathCalculationsUtil = new MathCalculationsUtil();
-        String pkgName = getIntent().getStringExtra("pkg");
+        pkgName = getIntent().getStringExtra("pkg");
 
         String appVersion = applicationUtility.getAppInformation(pkgName, StringsAnnotations.APP_VERSION);
         String appName = applicationUtility.getAppName(pkgName);
-        String appInstalledTime = timeUtil.appInstalledTime(applicationUtility.getAppInstallTime(pkgName,false));
-        String appLastUpdateTime = timeUtil.appInstalledTime(applicationUtility.getAppInstallTime(pkgName,true));
-        Drawable iconDrawable = applicationUtility.getAppInformation(pkgName,StringsAnnotations.APP_ICON);
+        String appInstalledTime = timeUtil.appInstalledTime(applicationUtility.getAppInstallTime(pkgName, false));
+        String appLastUpdateTime = timeUtil.appInstalledTime(applicationUtility.getAppInstallTime(pkgName, true));
+        Drawable iconDrawable = applicationUtility.getAppInformation(pkgName, StringsAnnotations.APP_ICON);
 
         tvAppName.setText(appName);
         tvVersion.setText(appVersion);
@@ -53,13 +61,37 @@ public class EachAppInfo extends AppCompatActivity {
         ivAppIcon.setImageDrawable(iconDrawable);
 
         try {
-            String appSize = mathCalculationsUtil.getCalculatedDataSizeWithPrefix((float)applicationUtility.getAppSize(pkgName));
+            String appSize = mathCalculationsUtil.getCalculatedDataSizeWithPrefix((float) applicationUtility.getAppSize(pkgName));
             tvSize.setText(appSize);
             Toast.makeText(this, "check", Toast.LENGTH_SHORT).show();
         } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
         }
 
+        llOpenApp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent launchIntent = getPackageManager().getLaunchIntentForPackage(pkgName);
+                startActivity( launchIntent );
+            }
+        });
+        llCheckOnStore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT,
+                        "https://play.google.com/store/apps/details?id=" + pkgName);
+                sendIntent.setType("text/plain");
+                startActivity(sendIntent);
+            }
+        });
+        llOpenSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
 
     }
